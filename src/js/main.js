45a635e60c19cb8c1ae667c329760f6299548608
Logan Leopold <loganleopld@me.com>
2019-01-11 15:25:25 -0500

    regretting squaring content before logic but too late now!

---
+++ src/js/main.js
@@ -0,0 +1,91 @@
+var startButton = document.getElementsByClassName('.buttonStartGame');
+
+startButton.addEventListener('click', startGame);
+ 
+ 
+function startGame () {
+
+    const questions = [
+
+          { questionNum: 1,
+            questionAsk: "How old was Ol' Blue Eyes when he passed?",
+            answer1: "62",
+            answer2: "72",
+            answer3: "82",
+          },
+
+
+          { questionNum: 2,
+            questionAsk: "Who sings the hook on Kendrick Lamar's 'Alright?'",
+            answer1: "Dr. Dre.",
+            answer2: "Pharrell Williams",
+            answer3: "The Weekend",
+          },
+
+          { questionNum: 3,
+            questionAsk: "Who is Gordon Matthew Thomas Sumner?",
+            answer1: "Sting",
+            answer2: "Nat King Cole",
+            answer3: "Prince",
+          },
+
+          { questionNum: 4,
+            questionAsk: "How old was Bono when U2 collectively agree they formed?",
+            answer1: "24",
+            answer2: "16",
+            answer3: "20",
+          },
+
+          { questionNum: 5,
+            questionAsk: "The Stradivarius is a make of which kind of instrument?",
+            answer1: "Guitar",
+            answer2: "Violin",
+            answer3: "Piano",
+          },
+
+          { questionNum: 6,
+            questionAsk: "How many keys are on the modern concert piano?",
+            answer1: "62",
+            answer2: "76",
+            answer3: "88",
+          },
+
+          { questionNum: 7,
+            questionAsk: "How did Joey Ramone pass away?",
+            answer1: "Lymphoma",
+            answer2: "Lung Cancer",
+            answer3: "Heart Disease",
+          },
+
+          { questionNum: 8,
+            questionAsk: "How many guitars are a part of John Mayer's collection?",
+            answer1: "755",
+            answer2: "200",
+            answer3: "50",
+          },
+
+          { questionNum: 9,
+            questionAsk: "While made famous by Jimi Hendrix, 'ALl Along The Watchtower' was originally written by who?",
+            answer1: "Bob Dylan",
+            answer2: "Marvin Gaye",
+            answer3: "The Everly Brothers",
+          },
+
+          { questionNum: 10,
+            questionAsk: "Ben Gibbert of Death Cab For Cutie is famous for what other hit song released through another project?",
+            answer1: "Transatlanticism",
+            answer2: "Such Great Heights",
+            answer3: "I Will Follow You Into The Dark",
+          },
+
+    ]
+
+    var count = 0
+
+    for (i = 0; i <= 10; i++) {
+
+     questions[i].   
+
+    }
+
+}
